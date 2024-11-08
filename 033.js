98c1fc3d20916381c2685f55fb3af174f22ff0a6
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-08 22:53:07 +0800

    non-trivial rationals' denominator . It is intriguing . Time has limit

---
+++ 033.js
@@ -0,0 +1,21 @@
+function isNonTrivial(num1,num2){
+
+}
+function digitCancellingFractions() {
+  const numeratorDenominator=[]
+ for(let d=11;d<100;d++)
+ {
+   for(let n=10;n<d;n++)
+   {
+      if(d%10==0 && n %10==0)
+      {
+        continue
+      }
+
+   }
+ }
+  return true;
+}
+
+digitCancellingFractions();
+//  16/64, 19/95, 26/65, and 49/98

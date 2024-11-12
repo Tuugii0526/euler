cf914fd7782357e99ef2e4102629103fc1a67649
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-12 18:49:26 +0800

    my solution is so expensive . Solution that takes less time intrigues my interest

--- 035.js
+++ 035.js
@@ -1,30 +1,54 @@
-function isPrime(number){
-    for(let i=2;i<number;i++)
+function isPrime(number) {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function getPermutations(numberArray) {
+  if (numberArray.length == 1) {
+    return numberArray;
+  }
+  const permutations = [];
+  for (let i = 0; i < numberArray.length; i++) {
+    let currNumber = numberArray[i];
+    const remainingNumbers = numberArray.filter(
+      (number,index) => index != i
+    );
+    const subPermutations= getPermutations(remainingNumbers)
+    subPermutations.forEach(permu=>{
+      permutations.push(`${currNumber}${permu}`)
+    })
+  }
+  return permutations
+}
+function isAllPermutationsPrime(number) {
+  const numberStringArray=String(number).split('')
+  const uniquePermutations=Array.from(new Set(getPermutations(numberStringArray)),(stringNumber)=>Number(stringNumber))
+for(let i=0;i<uniquePermutations.length;i++)
+{
+  if(!isPrime(uniquePermutations[i]))
+  {
+    return [false]
+  }
+}
+return [true,uniquePermutations]
+}
+function circularPrimes(n) {
+  const circularPrimesSet=new Set()
+  for(let i=2;i<n;i++)
+  {
+    if(isPrime(i) && !circularPrimesSet.has(i))
     {
-      if(i%number==0)
+      const booleanAndCirculations=isAllPermutationsPrime(i)
+      if(booleanAndCirculations[0])
       {
-        return false
+        circularPrimesSet.add(...booleanAndCirculations[1])
       }
     }
-    return true
-    }
-    function getPermutations(numberArray){
-     if(numberArray.length==1)
-     {
-       return numberArray
-     }
-     const permutations=[]
-     for(let i=0;i<numberArray.length;i++)
-     {
-       let currNumber=numberArray[i]
-       const remainingNumbers=numberArray.filter(number=>number!=currNumber)
-       
-     }
-    }
-    function isAllPermutationsPrime(){
-    
-    }
-    function circularPrimes(n) {
-      return n;
-    }
-    circularPrimes(1000000)
+  }
+  return Array.from(circularPrimesSet)
+}
+
+console.log(circularPrimes(100000))

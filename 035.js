28cbcb9a3101fd953f2656e8ecfecfae4e3a0e1b
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-08 17:05:59 +0800

    isPrime is learnt from teacher

--- 035.js
+++ 035.js
@@ -1,54 +1,48 @@
 function isPrime(number) {
-  for (let i = 2; i < number; i++) {
+  if (number == 1) {
+    return false;
+  }
+  if (number == 2 || number == 3) {
+    return true;
+  }
+  const sixMod = number % 6;
+  if (sixMod !== 5 && sixMod !== 1) {
+    return false;
+  }
+  return determineIsPrime(number);
+}
+function determineIsPrime(number) {
+  const limit = Math.sqrt(number);
+  for (let i = 3; i <= limit; i += 2) {
     if (number % i == 0) {
       return false;
     }
   }
   return true;
 }
-function getPermutations(numberArray) {
-  if (numberArray.length == 1) {
-    return numberArray;
-  }
-  const permutations = [];
-  for (let i = 0; i < numberArray.length; i++) {
-    let currNumber = numberArray[i];
-    const remainingNumbers = numberArray.filter(
-      (number,index) => index != i
-    );
-    const subPermutations= getPermutations(remainingNumbers)
-    subPermutations.forEach(permu=>{
-      permutations.push(`${currNumber}${permu}`)
-    })
-  }
-  return permutations
-}
+function getPermutations(numberArray) {}
 function isAllPermutationsPrime(number) {
-  const numberStringArray=String(number).split('')
-  const uniquePermutations=Array.from(new Set(getPermutations(numberStringArray)),(stringNumber)=>Number(stringNumber))
-for(let i=0;i<uniquePermutations.length;i++)
-{
-  if(!isPrime(uniquePermutations[i]))
-  {
-    return [false]
+  const uniquePermutations = [];
+  for (let i = 0; i < uniquePermutations.length; i++) {
+    if (!isPrime(uniquePermutations[i])) {
+      return [false];
+    }
   }
-}
-return [true,uniquePermutations]
+  return [true, uniquePermutations];
 }
 function circularPrimes(n) {
-  const circularPrimesSet=new Set()
-  for(let i=2;i<n;i++)
-  {
-    if(isPrime(i) && !circularPrimesSet.has(i))
-    {
-      const booleanAndCirculations=isAllPermutationsPrime(i)
-      if(booleanAndCirculations[0])
-      {
-        circularPrimesSet.add(...booleanAndCirculations[1])
+  const circularPrimesSet = new Set();
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i)) {
+      if (!circularPrimesSet.has(i)) {
+        const booleanAndCirculations = isAllPermutationsPrime(i);
+        if (booleanAndCirculations[0]) {
+          circularPrimesSet.add(...booleanAndCirculations[1]);
+        }
       }
     }
   }
-  return Array.from(circularPrimesSet)
+  return Array.from(circularPrimesSet);
 }
 
-console.log(circularPrimes(100000))
+console.log(circularPrimes(100000));
